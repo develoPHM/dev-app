95b54906b0c9c41b59d6dbf6be557d655817ca3f
develoPHM <phmok1@naver.com>
2025-12-15 16:11:26 +0900

    [Feat] 기본 GET 추가

---
+++ src/main/java/com/example/k3s/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.k3s.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "hello k3s";
+    }
+}
